efa3021e0f559e68e75f75eadf28ceef5ea3a6b5
ParulAwal <163215823+ParulAwal@users.noreply.github.com>
2024-06-29 23:24:52 -0700

    Components Added

---
+++ javascript/nav-bar.js
@@ -0,0 +1,14 @@
+const menuHam = document.querySelector('.menuBtn')
+const navBar = document.querySelector('.navbar')
+
+menuHam.addEventListener('click', function(){
+    document.body.classList.toggle('search-active')
+})
+
+
+// menuToggle.addEventListener('click', function(){
+//     document.body.classList.toggle('search-active')
+// })
+// menuItem.addEventListener('click', function(){
+//     document.body.classList.toggle('search-active')
+// })
