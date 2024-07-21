7c54ffbc387898f6a5ffd00c2df86858756c762c
rootxces1 <kapilpokhri9@gmail.com>
2024-07-21 12:39:05 -0700

    more info added in the organiser profile section

--- javascript/account-organiser.js
+++ javascript/account-organiser.js
@@ -9,8 +9,10 @@ const footerElement = document.querySelector('footer');
 setupHeaderFooter();
 
 const editBtn = document.getElementById('editProfile');
+const name = document.getElementById('name');
 const profilePic = document.getElementById('profile-image');
 const summary = document.getElementById('summary');
+const role = document.getElementById('role');
 const phone = document.getElementById('phone');
 const address = document.getElementById('address');
 
@@ -23,8 +25,18 @@ async function setUserDataOnUI() {
         if(user.profile_image) {
             profilePic.src = user.profile_image;
         }
+        if(user.fName) {
+            if(user.lName) {
+                name.textContent = `${user.fName} ${user.lName}`;
+            } else {
+                name.textContent = user.fName;
+            }
+        }
         if(user.summary) {
             summary.textContent = user.summary;
+        }
+        if(user.role) {
+            role.textContent = user.role;
         }
         if(user.phone) {
             phone.textContent = user.phone;
