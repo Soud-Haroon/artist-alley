8acc79c99b8b1eabd7642b1e9f0d8a46e1efb1b4
Soud <78749228+Soud-Haroon@users.noreply.github.com>
2024-07-24 00:14:02 -0700

    homepage and search bar fixes

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -49,6 +49,7 @@ function setDataOnUI() {
     const searchButton = document.getElementById('searchBtn');
 
     const navSearch = document.getElementById('nav-search');
+    const searchCloseBtn = document.getElementById('searchCloseBtn');
 
     navSearch.addEventListener('click', (event) => {
         event.preventDefault();
@@ -56,13 +57,19 @@ function setDataOnUI() {
         // alert("Welcom from inside!")
     })
 
+    searchCloseBtn.addEventListener('click', (event) => {
+        console.log("1==========================");
+        event.preventDefault();
+        document.body.classList.remove("search-active")
+        console.log("==========================");
+    })
 
     searchButton.addEventListener('click', (event) => {
         event.preventDefault();
         const query = searchInput.value.trim();
         if (query !== '') {
             let url = `../html/search-result.html?query=${query}`;
-            
+
 
             document.body.classList.toggle("searchactive");
             console.log(`url is: ${url}`)
