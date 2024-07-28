f3fbd202bedb1873f22cf7c017fb3ba12d0862e9
Soud <78749228+Soud-Haroon@users.noreply.github.com>
2024-07-28 00:15:54 -0700

    efix in env

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 // const firebaseConfig = firebase_ApiKey;
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
